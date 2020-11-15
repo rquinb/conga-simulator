0c50b084459799ca29c21de2d54fbec14d5a3375
power.user <power.user@user.com>
2020-11-15 15:32:45 -0300

    Bug fixes + endpoint for generating a deck

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import entities
 
 
 app = flask.Flask(__name__)
@@ -6,6 +7,11 @@ app = flask.Flask(__name__)
 @app.route('/')
 def home_page():
     return "conga-simulator v0.0.1"
+
+@app.route('/deck', methods=['GET'])
+def get_deck():
+    deck = entities.Deck()
+    return flask.jsonify({"cards": [card.to_string() for card in deck.cards]})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- entities.py
+++ entities.py
@@ -7,24 +7,31 @@ MAX_COMMODINES = 2
 
 class Card:
     def __init__(self, number=None, kind=None, commodin=False):
-        self.number = self._set_number(number)
-        self.kind = self._set_kind(kind)
+        self.number = self._set_number(number) if number else number
+        self.kind = self._set_kind(kind) if kind else kind
         self.commodin= commodin
 
     def _set_number(self, number):
         if number <= MAX_NUMBER:
-            self.number = number
+            return number
         else:
             raise game_exceptions.InvalidCardNumber(f"Please provide a number less than or equal to {MAX_NUMBER}")
 
     def _set_kind(self, kind):
         if kind in KINDS:
-            self.kind = kind
+            return kind
         else:
             raise game_exceptions.InvalidCardKind(f"Please provide a valid kind: It should be one of {KINDS}")
 
+    def to_string(self):
+        return f"{self.number} of {self.kind}" if not self.commodin else "Commodin"
+
+    def __str__(self):
+        return self.to_string()
+
     def __eq__(self, other_card):
         return self.number == other_card.number and self.kind == other_card.kind
+
 
 
 class Hand:
@@ -61,11 +68,12 @@ class Deck:
         for _ in range(MAX_COMMODINES):
             cards.append(Card(commodin=True))
 
-        self.cards = self._shuffle_cards(cards)
+        self._shuffle_cards(cards)
+        return cards
 
     @staticmethod
     def _shuffle_cards(cards):
-        return random.shuffle(cards)
+        random.shuffle(cards)
 
     def shuffle_deck(self):
-        self.cards = self._shuffle_cards(self.cards)
+        self._shuffle_cards(self.cards)
